842f98b640248ebb4a628ab7f6c122ac1651bb35
NedretaShukrieva <neti.shukri@gmail.com>
2026-06-08 20:19:53 +0300

    added game exceptions

---
+++ GameExceptions/GameException.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+
+class GameException : public std::runtime_error {
+public:
+    GameException(const std::string& message)
+        : std::runtime_error(message) {}
+};
+
+class InvalidProductException : public GameException {
+public:
+    InvalidProductException()
+        : GameException("Invalid product!") {
+    }
+};
+
+class InvalidUsernameException : public GameException {
+public:
+    InvalidUsernameException()
+        : GameException("Username is already taken!") {
+    }
+};
+
+class InvalidPasswordException : public GameException {
+public:
+    InvalidPasswordException()
+        : GameException("Password must have at least 3 symbols!") {
+    }
+};
+
+class InvalidUserTypeException : public GameException {
+public:
+    InvalidUserTypeException()
+        : GameException("Invalid user type!") {
+    }
+};
