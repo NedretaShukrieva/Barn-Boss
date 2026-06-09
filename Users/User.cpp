8e5a8d31c035cea9eaef0ecbf9cb3831be2b8ba8
NedretaShukrieva <neti.shukri@gmail.com>
2026-06-09 11:58:02 +0300

    added User base class

---
+++ Users/User.cpp
@@ -0,0 +1,67 @@
+#include "User.h"
+#include "System.h"
+#include <iostream>
+#include <print>
+#include <stdexcept>
+
+unsigned int User::countOfUsers = 0;
+
+std::ostream& User::serializeBase(std::ostream& os) const
+{
+    os << id << " " << username << " " << password << " ";
+    return os;
+}
+
+User::User(const std::string& username, const std::string& password):username(username), password(password), id(++countOfUsers)
+{
+   
+}
+
+void User::setId(unsigned int newId)
+{
+    id = newId;
+}
+
+const std::string& User::getUsername() const
+{
+    return username;
+}
+
+const std::string& User::getPassword() const
+{
+    return password;
+}
+
+unsigned int User::getId() const
+{
+    return id;
+}
+
+
+bool User::changePassword(const std::string& oldPassword, const std::string& newPassword)
+{
+    if (password != oldPassword) {
+        std::println("Wrong password!");
+        return false;
+    }
+
+    password = newPassword;
+
+    std::println("Password is changed successfully!");
+
+    return true;
+}
+
+void User::setCountOfUsers(unsigned int count)
+{
+    countOfUsers = count;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const User& user)
+{
+    user.serialize(os);
+    return os;
+}
+
+
---
+++ Users/User.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include "UserValidator.h"
+#include <iostream>
+
+class User
+{
+private:
+
+	static unsigned int countOfUsers;
+	
+	unsigned int id;
+	std::string username;
+	std::string password;
+
+protected:
+
+	std::ostream& serializeBase(std::ostream& os) const;
+
+public:
+
+	User() = default;
+	User(const std::string& username, const std::string& password);
+
+	void setId(unsigned int newId);
+	static void setCountOfUsers(unsigned int count);
+
+	const std::string& getUsername()const;
+	const std::string& getPassword()const;
+	unsigned int getId() const;
+
+	bool changePassword(const std::string& oldPassword, const std::string& newPassword);
+
+	virtual void profileInfo()const = 0;
+
+	virtual std::istream& deserialize(std::istream& is) = 0;
+	virtual std::ostream& serialize(std::ostream& os) const = 0;
+
+	virtual ~User() = default;
+
+	friend std::ostream& operator<<(std::ostream& os, const User& user);
+	
+};
+
